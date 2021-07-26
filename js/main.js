4ec55f7d93f6a2bdcc5f789c7865b4e536df35f3
Mariano <marianomarchionna8@gmail.com>
2021-07-26 16:47:34 +0200

    Verifica se l'utente ha perso o vinto

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-
-
 // Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 // I numeri non possono essere duplicati.
 var bombe = [];
@@ -20,18 +15,34 @@ while (bombe.length < numeroBombe) {
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 var numeriUtente = [];
 var ricercaNumeri = false;
+var ricercaBomba = false;
 
-
-while (numeriUtente.length < (cento - numeroBombe)) {
+while (numeriUtente.length < (cento - numeroBombe) && ricercaBomba == false) {
     do {  
-        var userNumber = parseInt(prompt('Inserisci un numero da 1 a 100'));
-    } while ((userNumber < 1) || (userNumber > cento) || (isNaN(userNumber)));
+        var numero = parseInt(prompt('Inserisci un numero da 1 a 100'));
+    } while (numero < 1 || numero > cento || isNaN(numero));
 
-    ricercaNumeri = isInArray(numeriUtente, userNumber);
-    numeriUtente.push(userNumber);
+    ricercaNumeri = isInArray(numeriUtente, numero);
+    ricercaBomba = isInArray(bombe, numero);
+
+    if (ricercaBomba == false){
+        numeriUtente.push(numero);
+    } else if (ricercaNumeri == true){
+        alert('Inserisci un altro numero, questo è già stato inserito!');
+    } else {
+        alert('BOOOM! Hai colpito una mina, mi dispiace, hai perso!');
+    }
+    i++;
 }
+
+if (numeriUtente.length == cento - numeroBombe){
+    alert('Complimenti, hai vinto!');
+}
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 // Funzioni
 function randomNumber(min, max) {
