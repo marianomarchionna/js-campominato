7ec5df35e02e34b5df84a91f97efc1cf7e3e3e08
Mariano <marianomarchionna8@gmail.com>
2021-07-26 16:15:42 +0200

    Generazione casuale bombe non duplicate

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+// Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
+// I numeri non possono essere duplicati.
+var bombe = [];
+const numeroBombe = 16;
+const cento = 100;
+var i = 0;
+
+while (bombe.length < numeroBombe) {
+    let bomba = randomNumber(1, cento);
+    let ricerca = isInArray(bombe, bomba);
+    if (ricerca == false) 
+        bombe.push(bomba);
+    i++;
+}
+
+console.log(bombe);
+
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+
+
+// Funzioni
+function randomNumber(min, max) {
+    return risultato = Math.floor(Math.random() * max - min + 1) + min; 
+}
+
+function isInArray(array, element) {
+    var result = false;
+    var i = 0;
+    while (i < array.length && result == false) {
+
+        if (element == array[i])
+            result = true;
+
+        i++;
+    }
+    return result;
+}
